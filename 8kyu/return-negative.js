968ee4d32486debeace835770b52312d1cd4347b
Dmitrii599 <osipowy@yandex.ru>
2025-02-18 11:51:31 +0400

    return-negative

---
+++ 8kyu/return-negative.js
@@ -0,0 +1,16 @@
+// Description:
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Solution:
+// function makeNegative(num) {
+// return Math.abs(num) * -1;
+
+function makeNegative(num){
+    // Code?
+    return Math.abs(num) * -1;
+}
+
+console.log(makeNegative(-1));
+console.log(makeNegative(-5));
+console.log(makeNegative(0));
+console.log(makeNegative(-0.12));
