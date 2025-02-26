bfe5471529c764b0d5d9772827c668b0e36be535
Dmitrii599 <osipowy@yandex.ru>
2025-02-26 11:19:23 +0400

    get-the-me-of-an-array.js

---
+++ 8kyu/get-the-me-of-an-array.js
@@ -0,0 +1,29 @@
+// ❓ Description:
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+
+// Test.assertEquals(getAverage([2,2,2,2]),2);
+// Test.assertEquals(getAverage([1,2,3,4,5,]),3);
+// Test.assertEquals(getAverage([1,1,1,1,1,1,1,2]),1);
+
+// ‼️Best solution:
+// function getAverage(marks){
+// return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+// }
+
+// ❗ My solution:
+function getAverage(marks){
+    //TODO : calculate the downward rounded average of the marks array
+    let result = 0;
+    for (let i = 0; i < marks.length; i++) {
+        result += marks[i] / marks.length;
+    }
+    return Math.floor(result);
+}
+let arr1 = [2, 2, 2, 2];
+let arr2 = [1, 2, 3, 4, 5];
+let arr3 = [1, 1, 1, 1, 1, 1, 1, 2];
+
+console.log(getAverage(arr1));
+console.log(getAverage(arr2));
+console.log(getAverage(arr3));
